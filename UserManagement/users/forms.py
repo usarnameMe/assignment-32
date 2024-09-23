d80de4ae56ecc9df661b11c72bfac78ba80b83f0
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-09-23 17:10:26 +0400

    done

--- UserManagement/users/forms.py
+++ UserManagement/users/forms.py
@@ -3,6 +3,10 @@ from .models import User
 
 
 class UserForm(forms.ModelForm):
+    date_of_birth = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'date_of_birth']
