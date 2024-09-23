b899df8384100cdde0a09d63412a926a54783561
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-09-23 17:01:07 +0400

    first step done

---
+++ UserManagement/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class User(models.Model):
+    objects = None
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
